43cbf6025288cff1f8c291c16ad5749808d8ebea
Joe <oryx@umich.edu>
2016-08-10 15:43:27 -0400

    Finished! Needs testing!

--- DMFT_DataCollection.py
+++ DMFT_DataCollection.py
@@ -1,7 +1,18 @@
+from contextlib import contextmanager
 import os
 import subprocess as sp
 import argparse
 import json
+import math
+
+@contextmanager
+def cd(newdir):
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)
 
 #Soft equivalence for dictionaries, returns true iff for all entires in 'a', 'b' has at least the same entries. So 'b' could have additional entries as well!
 def equiv_dicts(a,b):
@@ -85,20 +96,48 @@ def get_param_sets(params):
 	
 	return param_sets
 	
+#Read the requested data from the provided file, at the indicated [line, pos] in param_set["ELEMENT"]
 def read_data(param_sets, target):
 	start_location = os.getcwd()
+	data_vals=[]
 	for jk_set in param_sets:
+		jk_data = []
 		for param_set in jk_set:
 			target_dir = param_set["Location"]
-			target_file = param_set["FILENAME"]
-			target_element = param_set["ELEMENT"]
+			print target_dir
+			target_file = target["FILENAME"]
+			target_element = target["ELEMENT"]
 			f = open(target_dir+"/"+target_file)
-			
+			line = f.readline()
+			for i in range(0, target_element[0]-1):
+				line = f.readline()
+			line=line.split()
+			line = [float(i) for i in line]
+			data = line[target_element[1]]
 			f.close()
-	return [[]]
+			jk_data.append(data)
+		data_vals.append(jk_data)
+	return data_vals
 	
 def exec_data(param_sets, target):
-	return [[]]
+	start_location = os.getcwd()
+	data_vals = []
+	for jk_set in param_sets:
+		jk_data = []
+		for param_set in jk_set:
+			target_dir = param_set["Location"]
+			print target_dir
+			target_prog = target["PROG"]
+			with cd(target_dir):
+				sp.call(target_prog+" > energy.dat",shell=True)
+				f=open("energy.dat","r")
+				data = f.readline()
+				f.close()
+			data = data.split()
+			data = [float(i) for i in data]
+			jk_data.append(data)
+		data_vals.append(jk_data)
+	return data_vals
 	
 def acquire_data(param_sets, target):
 	method = target["TYPE"]
@@ -110,6 +149,107 @@ def acquire_data(param_sets, target):
 		print "Error! Unknown data acquisition type!"
 		print target
 		return [[]]
+	
+def calculate_sum(data, partial, index):
+	num_iter = len(data)
+	num_data = len(data[0])
+	#print data
+	if(type(data[0][0]) is list):
+		result = []
+		for i in range(0, num_data):
+			result.append([0]*len(data[0][0]))
+	else:
+		result = [0]*num_data
+	
+	if(partial):
+		M=num_iter-1
+	else:
+		M=num_iter
+	
+	for i in range(0, num_iter):
+		if(partial and i == index):
+			continue
+		for x in range(0, num_data):
+			val = data[i][x]
+			if(type(val) is list):
+				for y in range(0, len(val)):
+					result[x][y] += (1./M)*val[y]
+			else:
+				result[x] += (1./M)*val
+
+	return result
+	
+def calculate_mean_error_jackknife(data_ave, data_bar, data_partialsums):
+	num_iter = len(data_partialsums)
+	num_data = len(data_partialsums[0])
+	is_list = False
+	len_list = 0
+	if(type(data_ave[0]) is list):
+		is_list = True
+		len_list = len(data_ave[0])
+		result = []
+		error = []
+		partialsquare = []
+		for i in range(0, num_data):
+			result.append([0]*len_list)
+			error.append([0]*len_list)
+			partialsquare.append([0]*len_list)
+	else:
+		result = [0]*num_data
+		error = [0]*num_data
+		partialsquare = [0]*num_data
+		
+	#Calculate JK mean
+	for i in range(0, num_data):
+		if(is_list):
+			for j in range(0, len_list):
+				result[i][j] = data_ave[i][j] - (num_iter-1)*(data_bar[i][j] - data_ave[i][j])
+		else:
+			result[i] = data_ave[i] - (num_iter-1)*(data_bar[i] - data_ave[i])
+			
+	#Calculate partialsum squares for JK error
+	for i in range(0, num_iter):
+		for j in range(0, num_data):
+			if(is_list):
+				for k in range(0, len_list):
+					partialsquare[j][k] += data_partialsums[i][j][k]*data_partialsums[i][j][k]
+			
+			else:
+				partialsquare[j] += data_partialsums[i][j]*data_partialsums[i][j]
+				
+	#Calculate JK error
+	for i in range(0, num_data):
+		if(is_list):
+			for j in range(0, len_list):
+				error[i][j] = math.sqrt(num_iter-1)*math.sqrt(abs(partialsquare[i][j]/num_iter-data_bar[i][j]*data_bar[i][j]))
+		else:
+			error[i] = math.sqrt(num_iter-1)*math.sqrt(abs(partialsquare[i]/num_iter-data_bar[i]*data_bar[i]))
+	
+		
+	return (result, error)
+	
+def jackknife(data):
+	#Calculate the jackknife mean and error
+	if(not len(data) > 1):
+		print "Error! Need more that 1 data set to do jackknife analysis"
+		return ([],[],[])
+	num_iter = len(data)
+	num_data = len(data[0])
+	print "Performing jackknife analysis on "+str(num_iter)+" iterations, each with "+str(num_data)+" elements."
+	
+	data_ave = []
+	data_bar = []
+	data_partialsums = [[]]*num_iter
+	
+	#Calculate all the sums needed for jackknife
+	data_ave = calculate_sum(data, False, 0)
+	for i in range(0, num_iter):
+		data_partialsums[i] = calculate_sum(data, True, i)
+	data_bar = calculate_sum(data_partialsums, False, 0)
+	
+	(mean_jk, error_jk) = calculate_mean_error_jackknife(data_ave, data_bar, data_partialsums)
+	
+	return (data_ave, mean_jk, error_jk)
 
 def main():
 	print "I collect data!"
@@ -120,10 +260,12 @@ def main():
 	#Get the name of the json file containing the parameters
 	parser = argparse.ArgumentParser(description='Creates a batch file and parameter files that will perform a series of calculations for the given parameters')
 	parser.add_argument('input_file',help='JSON file containing the parameters')
+	parser.add_argument('output_file_prefix',help='Prefix for data output files')
 	args = parser.parse_args()	
 	
 	#Open and read the json file
 	in_filename = args.input_file
+	out_fileprefix = args.output_file_prefix
 	print 'Reading parameter file: ',in_filename
 	json_data = open(in_filename)
 	params = json.load(json_data)	
@@ -148,14 +290,43 @@ def main():
 	print "Found the parameter sets"
 	#print param_sets
 	
+	#Get the variable parameters (x-axis)
+	var_param_name = params["Variable"].keys()[0]
+	var_params = params["Variable"][var_param_name]
+	print "Variable param: "+var_param_name
+	print var_params
+	
 	#Read or calculate the requested data
 	print "Acquiring data"
 	for data_target in params["Data"].keys():
 		print "Getting "+data_target
+		output_file = out_fileprefix+data_target + ".dat"
 		data = acquire_data(param_sets, params["Data"][data_target])
-		print data
-	
-	
+		
+		if(len(data[0]) == 0):
+			continue
+		elif(len(data) > 1):
+			(mean, results, jk_error) = jackknife(data)
+		else:
+			(mean, results, jk_error) = (data[0], [0]*len(data[0]), [0]*len(data[0]))
+		print output_file
+		print "Results: ",results
+		print "Error: ", jk_error
+		f = open(output_file, 'w')
+		f.write(var_param_name+"\t"+data_target+" (mean)\t"+data_target+" (jk)\tError\n")
+		for i in range(0, len(results)):
+			f.write(str(var_params[i])+"\t")
+			if(type(mean[i]) is list):
+				for x in mean[i]:
+					f.write(str(x)+"\t")
+				for x in results[i]:
+					f.write(str(x)+"\t")
+				for x in jk_error[i]:
+					f.write(str(x)+"\t")
+			else:
+				f.write(str(mean[i])+"\t"+str(results[i])+"\t"+str(jk_error[i])+"\t")
+			f.write("\n")
+		f.close()	
 	
 	
 if __name__ == "__main__":
