ae47b5aebfd3edc1c256ed55ff7082f9055571fa
Joe <oryx@umich.edu>
2016-09-19 14:11:33 -0400

    Added short script to generate TBC grid for DMFT_JobManager.py

---
+++ Generate_TBC_Grid2D.py
@@ -0,0 +1,14 @@
+import math
+import numpy
+
+L = 7
+IncEndPt = True
+MaxPhase = 4*math.pi
+
+theta = numpy.linspace(0, MaxPhase, L, IncEndPt)
+thetas = [(x,y) for x in theta for y in theta]
+PHASE_0 = [x[0] for x in thetas]
+PHASE_1 = [x[1] for x in thetas]
+
+print('"PHASE_0": '+ str(PHASE_0)+",")
+print('"PHASE_1": '+str(PHASE_1))  
